857f01f8df2a2ede9dfaccf51d8c9699be2dd3ec
Onirim <20838877+Onirim@users.noreply.github.com>
2026-03-23 13:18:54 +0100

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
 // ── Config Supabase partagée ──────────────────────────────────
-const SUPABASE_URL = 'https://oovnbcksdgjmrxusqwrk.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_HvCLP_mM4UfhEmtEFLJAaA_7Xm2ql5i';
+const SUPABASE_URL = 'https://XXXXXXXXXXXX.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_XXXXXXXXXXXXXXXX';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
